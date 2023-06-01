502d03be408fa89500af56be8c3ac42a3833a6fb
aditya-sengupta <adityars@ucsc.edu>
2023-06-01 12:44:58 -0700

    reorg and better seal scripting

--- src/lantern_reader.py
+++ src/lantern_reader.py
@@ -2,10 +2,15 @@ import numpy as np
 import imageio.v3 as iio
 
 from astropy.stats import sigma_clipped_stats
+from math import floor, ceil
 from matplotlib import pyplot as plt
 from os import path
 from photutils.detection import DAOStarFinder
 from scipy.spatial import ConvexHull
+
+PROJECT_ROOT = path.dirname(path.abspath(__file__))
+if "src" in PROJECT_ROOT or "scripts" in PROJECT_ROOT:
+    PROJECT_ROOT = path.dirname(PROJECT_ROOT)
 
 def angles_relative_to_center(x, y):
     xc, yc = np.mean(x), np.mean(y)
@@ -13,7 +18,7 @@ def angles_relative_to_center(x, y):
     return (np.arctan2(yd, xd) + np.pi / 2) % (2 * np.pi)
 
 class LanternReader:
-    def __init__(self, nports, cutout_size, fwhm, ext, imgshape, guess_positions=[]):
+    def __init__(self, nports, cutout_size, fwhm, ext, imgshape, guess_positions=[], subdir=""):
         self.nports = nports
         self.cutout_size = cutout_size
         self.ext = ext
@@ -21,35 +26,82 @@ class LanternReader:
         self.imgshape = imgshape
         self.yi, self.xi = np.indices(imgshape)
         self.guess_positions = guess_positions
+        self.subdir = subdir
 
-    def read_image(self, p):
-        return iio.imread(path.join("..", "data", p + "." + self.ext))[:,:,0]
+    @property
+    def directory(self):
+        return path.join(PROJECT_ROOT, "data", self.subdir)
+
+    def filepath(self, fname, ext=None):
+        """
+        The path we want to save/load data from or to.
+        """
+        if ext is None:
+            ext = self.ext
+        return path.join(PROJECT_ROOT, "data", self.subdir, fname + "." + ext)
+
+    def read_image(self, fname):
+        path_to_file = self.filepath(fname)
+        if self.ext == "raw":
+            return np.fromfile(path_to_file, dtype="int16", sep="").reshape(self.imgshape)
+        elif self.ext == "npy":
+            return np.load(path_to_file)
+        else:
+            img = iio.imread(path_to_file)
+            if len(img.shape) == 3:
+                return img[:,:,0]
+            return img
     
-    def set_centroids(self, img, min_energy=10000):
+    def set_centroids(self, img, min_energy=10000, imax=50):
         # initial finding
+        """
         imgc = img.copy()
         locs = []
         i = 0
         s = self.cutout_size
-        while len(self.guess_positions) < self.nports and not(np.all(imgc == 0)):
+        while len(self.guess_positions) < self.nports and i < imax:
             ind = np.unravel_index(np.argmax(imgc, axis=None), img.shape)
             if np.sum(imgc[ind[0]-s:ind[0]+s, ind[1]-s:ind[1]+s]) > min_energy:
                 self.guess_positions.append(ind)
-                imgc[ind[0]-s:ind[0]+s,ind[1]-s:ind[1]+s] = 0
-        
-        # COM refinement
+            imgc[ind[0]-s:ind[0]+s,ind[1]-s:ind[1]+s] = 0
+            i += 1
+        """
+        i = 50
+        if i == imax:
+            mean, median, std = sigma_clipped_stats(img, sigma=10.0)
+            daofind = DAOStarFinder(fwhm=self.fwhm, threshold=25.*std) 
+            # fwhm tuned to the port sizes
+            # threshold is just large relative to backgrouhd
+            sources = daofind(img - median)
+            xc, yc = (np.asarray(sources[k]) for k in ["xcentroid", "ycentroid"])
+            assert len(xc) == self.nports, f"should have found {self.nports} ports but found {len(xc)}"
+            refined_locs = np.vstack((xc, yc)).T
+
+        """# COM refinement
         refined_locs = []
         for (xc, yc) in self.guess_positions:
             img_cut, xi_cut, yi_cut = img[xc-s:xc+s,yc-s:yc+s], self.xi[xc-s:xc+s,yc-s:yc+s], self.yi[xc-s:xc+s,yc-s:yc+s]
             xcom = np.sum(xi_cut * img_cut) / np.sum(img_cut)
             ycom = np.sum(yi_cut * img_cut) / np.sum(img_cut)
-            refined_locs.append([xcom, ycom])
+            refined_locs.append([xcom, ycom])"""
 
-        self.xc, self.yc = self.ports_in_radial_order(np.array(refined_locs))
+        self.xc, self.yc, self.radial_shell = self.ports_in_radial_order(np.array(refined_locs))
+
+    def plot_ports(self):
+        # only run after set_centroids
+        plt.scatter(self.xc, self.yc, c=self.radial_shell)
+        plt.xticks([])
+        plt.yticks([])
+        for (i, (xc, yc)) in enumerate(zip(self.xc, self.yc)):
+            plt.annotate(i + 1, (xc, yc), xytext=(xc+5, yc+5))
+
+        plt.show()
 
     def ports_in_radial_order(self, points):
         xnew, ynew = np.zeros_like(points[:,0]), np.zeros_like(points[:,1])
         prev_idx = 0
+        radial_shell = np.zeros(len(xnew))
+        k = 0
         while len(points) > 0: # should run three times for a 19-port lantern
             if len(points) == 1:
                 v = np.array([0])
@@ -61,22 +113,42 @@ class LanternReader:
             sortperm = np.argsort(angles_relative_to_center(xtemp, ytemp))[::-1]
             xnew[prev_idx:prev_idx+nhull] = xtemp[sortperm]
             ynew[prev_idx:prev_idx+nhull] = ytemp[sortperm]
+            radial_shell[prev_idx:prev_idx+nhull] = k
+            k += 1
             prev_idx += nhull
             points = np.delete(points, v, axis=0)
 
-        return np.flip(xnew), np.flip(ynew)
+        return np.flip(xnew), np.flip(ynew), np.flip((k - 1 - radial_shell) / (k - 1))
 
-    def get_intensities(self, img, fwhm=15):
+    def bounding_box(self, pad=5):
+        return (floor(min(self.xc)-pad), ceil(max(self.xc)+pad), floor(min(self.yc)-pad), ceil(max(self.yc)+pad))
+
+    def crop_to_bounding_box(self, img, savename=None, pad=5):
+        bbox = self.bounding_box(pad=pad)
+        if len(img.shape) == 3:
+            crop_img = img[:,bbox[2]:bbox[3], bbox[0]:bbox[1]]
+        else:
+            crop_img = img[bbox[2]:bbox[3], bbox[0]:bbox[1]]
+
+        if savename is not None:
+            np.save(self.filepath(fname, ext="npy"), crop_img)
+
+        return crop_img
+
+    def get_intensities(self, img):
         mask = np.zeros_like(img)
         intensities = np.zeros(self.nports)
         for (i, (xv, yv)) in enumerate(zip(self.xc, self.yc)):
-            intensities[i] = np.average(img[np.where((self.xi - xv) ** 2 + (self.yi - yv) ** 2 <= fwhm ** 2)])
+            intensities[i] = np.average(img[np.where((self.xi - xv) ** 2 + (self.yi - yv) ** 2 <= self.fwhm ** 2)])
 
         return intensities
 
-    def reconstruct_image(self, img, intensities, fwhm=15):
+    def reconstruct_image(self, img, intensities):
         recon_image = np.zeros_like(img)
         for (xv, yv, intensity) in zip(self.xc, self.yc,intensities):
-            recon_image[np.where((self.xi - xv) ** 2 + (self.yi - yv) ** 2 <= fwhm ** 2)] = intensity
+            recon_image[np.where((self.xi - xv) ** 2 + (self.yi - yv) ** 2 <= self.fwhm ** 2)] = intensity
 
         return recon_image
+
+    def save(self, fname, data, ext="npy"):
+        np.save(self.filepath(fname, ext=ext), data)
