9169beef17884565041be6904ddc4b6ff87b43f7
aditya-sengupta <adityars@ucsc.edu>
2023-08-16 10:28:29 -0700

    ready to close loop

--- scripts/seal/pl_closeloop.py
+++ scripts/seal/pl_closeloop.py
@@ -11,11 +11,23 @@ def pl_interaction_matrix(dm, cam):
     N = reader.nports
     A = np.zeros((N, N))
     for z in trange(reader.nports):
-        for amp in [-poke_amplitude, poke_amplitude]:
-            dm.pokeZernike(amp, z + 2)
-            A[z, :] += reader.get_intensities(cam.get())
-    A = A / 2
+        for s in [-1, 1]:
+            dm.pokeZernike(s * amp, z + 2)
+            A[z, :] += (s / 2) * reader.get_intensities(cam.get())
     return A
 
-def closeloop(gain=0.9, leak=0.99):
-    pass
+def pl_command_matrix(dm, cam):
+    IM = pl_interaction_matrix(dm, cam)
+    return np.linalg.pinv(IM, rcond=1e-6)
+
+# probably check if the command matrix evaluated at the flat position is all ~zero
+
+def close_loop(dm, cam, CM, niter=10, gain=0.9, leak=0.99):
+    prev_dmc = np.zeros(reader.nports)
+    for _ in range(niter):
+        measurement = reader.get_intensities(cam.get())
+        dmc = gain * (CM @ measurement) + leak * prev_dmc
+        prev_dmc = dmc
+        dm.pokeZernike(0, 0)
+        for (z, d) in enumerate(dmc):
+            dm.pokeZernike(z + 2, d, bias=dm.current_surf)
