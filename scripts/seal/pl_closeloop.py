2ed1a6d74b0d4579b470f7d785362ac64d5ca5dc
aditya-sengupta <adityars@ucsc.edu>
2023-10-12 17:08:39 -0700

    Mixed-Zernike PL closed-loop attempts

--- scripts/seal/pl_closeloop.py
+++ scripts/seal/pl_closeloop.py
@@ -32,6 +32,8 @@ input("Turn on the laser!")
 
 calibrate = lambda dm: plwfs.calibrate(dm, amp_calib, reader.nports)
 
+# turb = atm(1.5, 635e-9, 0.75, 5, 40, 1e-3, slm)
+
 def pl_controller(dm, gain=0.1):
     controller = integrator(plwfs, dm)
     controller.loopgain = gain
@@ -43,9 +45,39 @@ def pl_correct(dm, controller, amp, zern, niter=10):
     dm.newFlat(dm.pokeZernike(amp, zern))
     input("Start loop closing.")
     controller.closeLoop(niter)
-    input("Done, press any key to end. ")
+    print(plwfs.img2cmd(plwfs.getImage()))
+    input("Done, press Enter to end. ")
     dm.newFlat(true_flat)
 
+def pl_correct_slmzern(dm, controller, dm_slm, niter=10, limslm=0.3):
+    plwfs.update_flat(dm)
+    # u, s, v = np.linalg.svd(plwfs.cmdMat)
+    amp = np.random.uniform(-limslm, limslm, plwfs.reader.nports)
+    dm_slm.pokeZernike(
+        amp,
+        #u @ np.linalg.lstsq(u, amp, rcond=-1)[0], 
+        np.arange(2, 20)
+    )
+    init_wf_read = plwfs.img2cmd(plwfs.getImage())
+    input("Start loop closing.")
+    controller.closeLoop(niter)
+    print(plwfs.img2cmd(plwfs.getImage()) / init_wf_read)
+    input("Done, press Enter to end.")
+    slm.reset()
+    # dm_slm.pokeZernike(0.0, 1)
+    dm.pokeZernike(0.0, 1)
+
+# pl_correct_slmzern(dm_mems, controller, dm_slm)
+
+def pl_turb_correct(dm, controller, turb, slm, niter=10):
+    plwfs.update_flat(dm)
+    print(plwfs.img2cmd(plwfs.getImage()))
+    input("Start loop closing.")
+    controller.closeLoop(niter)
+    print(plwfs.img2cmd(plwfs.getImage()))
+    input("Done, press Enter to end.")
+    slm.reset()
+    dm.pokeZernike(0.0, 1)
 
 #print("Initialized camera, setting port positions")
 #reader.set_centroids(c.get(100))
